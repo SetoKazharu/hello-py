fdaa0ddb795d935258f3ece7efff445e58d60b4c
SetoKazharu <seto_kazharu@nate.com>
2017-01-21 19:35:05 +0900

    sum.py 추가됨

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+# sum.py에서 sum 함수를 불러옴 (import)
+from sum import sum
+
 print("Hello, World!")
+
+a = 1
+b = "으악!"
+
+print(a)
+print(b)
+
+# sum.py에 정의된 sum() 호출하기
+print(sum(1, 1000))
---
+++ sum.py
@@ -0,0 +1,5 @@
+def sum(x, y):
+    result = 0
+    for a in range(x, y+1):
+        result += a
+    return result
